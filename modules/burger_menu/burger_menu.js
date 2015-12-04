d25cbf6720dffa01e6bf663b78702d3fb3df15d6
Rosefalk <thomas_rosefalk@hotmail.com>
2015-12-04 13:08:43 +0100

    burger menu update
    added functions to instance for open/close add and remove items

--- modules/burger_menu/burger_menu.js
+++ modules/burger_menu/burger_menu.js
@@ -4,7 +4,63 @@ core.modules.burger_menu.instance = {
     moduleData : {
         location : "modules/burger_menu/",
         menuOpen: false,
-        onlyLoadIfMobile: false
+        onlyLoadIfMobile: false,
+        $container: undefined,
+        $list     : undefined
+    },
+    populate: function(json){
+        var self = this;
+        var $listItem = $(document.createElement("li"));
+            $listItem.html(json.text);
+        if(json.index=="first"){
+            self.moduleData.$list.prepend($listItem);
+        }
+        else if(json.index == undefined || json.index=="last"){
+            self.moduleData.$list.append($listItem);
+        }else{
+            var indx = json.index - 1;
+            console.log(indx)
+            if(indx==-1){
+                indx=0;
+                self.moduleData.$list.children("li").eq(indx).before($listItem);
+            }else{
+                self.moduleData.$list.children("li").eq(indx).after($listItem);
+            }
+        }
+
+    },
+    depopulate: function(json){
+        var self = this;
+        if(json.index=="first"){
+            self.moduleData.$list.children("li").first().remove();
+        }
+        else if(json.index == undefined || json == undefined || json.index=="last"){
+            self.moduleData.$list.children("li").last().remove();
+        }
+        else{
+            self.moduleData.$list.children("li").eq(json.index).remove();
+        }
+    },
+    flipBurger: function(bool){
+        var self = this;
+        var status;
+
+        if(bool != undefined){
+            status = bool;
+        }else{
+            status = !self.moduleData.menuOpen;
+        }
+
+        self.moduleData.menuOpen = status;
+
+        if(self.moduleData.menuOpen){
+            TweenMax.to(self.moduleData.$container,0.4,{left:0});
+        }else{
+            TweenMax.to(self.moduleData.$container,0.4,{
+                left:-self.moduleData.$container.width()
+            });
+        }
+
     },
     init: function(){
         var self = this;
@@ -20,8 +76,8 @@ core.modules.burger_menu.instance = {
             dataType: "html",
             success: function(data){
                 core.moduleData.$topUILayer.append(data)
-                var $container = $(".burger-menu-wrapper:last");
-
+                var $container = self.moduleData.$container = $(".burger-menu-wrapper:last");
+                self.moduleData.$list = $container.find(".burger-menu-list");
                 $container.css("left",-$container.width())
 
                 var clickType = "click";
@@ -30,15 +86,7 @@ core.modules.burger_menu.instance = {
                 }
 
                 $container.find(".burger-menu-icon").on(clickType,function(){
-
-                    var containerWidth = $container.width()
-
-                    if(self.moduleData.menuOpen){
-                        TweenMax.to($container,0.4,{left:-containerWidth});
-                    }else{
-                        TweenMax.to($container,0.4,{left:0});
-                    }
-                        self.moduleData.menuOpen = !self.moduleData.menuOpen;
+                    self.flipBurger();
                 });
             }
         });
